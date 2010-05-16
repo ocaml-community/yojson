7b203a22a3271c048c6015c85a5f5c9310593c27
mjambon <mjambon@6220f132-25dc-4409-94df-f78f6c84026c>
2010-05-16 08:11:30 +0000

    Added write benchmarks.

--- bench.ml
+++ bench.ml
@@ -11,16 +11,29 @@ let data =
     assert false
   with End_of_file -> String.concat "\n" (List.rev !l)
 
+let yojson_data = Yojson.Safe.from_string data
+let jsonwheel_data = Json_io.json_of_string data
+
 let n = 10_000
 
-let yojson_loop () =
+let yojson_rd_loop () =
   for i = 1 to n do
     ignore (Yojson.Safe.from_string data)
   done
 
-let jsonwheel_loop () =
+let yojson_wr_loop () =
+  for i = 1 to n do
+    ignore (Yojson.Safe.to_string yojson_data)
+  done
+
+let jsonwheel_rd_loop () =
   for i = 1 to n do
     ignore (Json_io.json_of_string data)
+  done
+
+let jsonwheel_wr_loop () =
+  for i = 1 to n do
+    ignore (Json_io.string_of_json ~compact:true jsonwheel_data)
   done
 
 let time msg f =
@@ -30,7 +43,13 @@ let time msg f =
   printf "%s: %.3f\n%!" msg (t2 -. t1)
 
 let () =
-  time "yojson" yojson_loop;
-  time "json-wheel" jsonwheel_loop;
-  time "yojson" yojson_loop;
-  time "json-wheel" jsonwheel_loop
+  time "rd yojson" yojson_rd_loop;
+  time "rd json-wheel" jsonwheel_rd_loop;
+  time "rd yojson" yojson_rd_loop;
+  time "rd json-wheel" jsonwheel_rd_loop;
+
+  time "wr yojson" yojson_wr_loop;
+  time "wr json-wheel" jsonwheel_wr_loop;
+  time "wr yojson" yojson_wr_loop;
+  time "wr json-wheel" jsonwheel_wr_loop
+  
