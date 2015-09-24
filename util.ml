c48c55fff20ad29a7549a58868c491d625d3ea05
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-09-23 18:27:46 -0700

    Defining |> is not needed anymore

--- util.ml
+++ util.ml
@@ -13,8 +13,6 @@ let typerr msg js = raise (Type_error (msg ^ typeof js, js))
 
 exception Undefined of string * json
 
-let ( |> ) x f = f x
-
 let assoc name obj =
   try List.assoc name obj
   with Not_found -> `Null
--- util.mli
+++ util.mli
@@ -68,10 +68,6 @@ exception Undefined of string * json
       return undefined. Currently this only happens when an array index is out
       of bounds. *)
 
-val ( |> ) : 'a -> ('a -> 'b) -> 'b
-  (** Forward pipe operator; useful for composing JSON access functions
-      without too many parentheses *)
-
 val keys : json -> string list
   (** Returns all the key names in the given JSON object *)
 
