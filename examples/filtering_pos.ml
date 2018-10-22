3e72885fa3de08bdfcb22fed758fcf23f435e1be
Takashi Suwa <bd.gfngfn@gmail.com>
2018-10-22 16:42:20 +0900

    update example

--- examples/filtering_pos.ml
+++ examples/filtering_pos.ml
@@ -28,30 +28,43 @@ EOF
 
 open Yojson.SafePos.Util
 
+let show ?print:prfopt (pos, a) =
+  let open Yojson.SafePos in
+  let fnamestr =
+    match pos.file_name with
+    | None -> ""
+    | Some(x) -> " in '" ^ x ^ "'"
+  in
+  let lnum1 = pos.start_line in
+  let lnum2 = pos.end_line in
+  let () =
+    match prfopt with
+    | None -> Printf.printf "*"
+    | Some(prf) -> Printf.printf "%a" prf a
+  in
+  if lnum1 = lnum2 then
+    Printf.printf " (line %d, column %d-%d%s)\n"
+      lnum1 pos.start_column pos.end_column fnamestr
+  else
+    Printf.printf " (line %d column %d to line %d, column %d)\n"
+      lnum1 pos.start_column lnum2 pos.end_column
+
 let extract_titles json =
-  [json]
-    |> filter_member "pages"
-    |> flatten
+  let objs =
+    [json]
+      |> filter_member "pages"
+      |> flatten
+  in
+  List.iter show objs;
+  objs
     |> filter_member "title"
     |> filter_string_with_pos
 
+let print_string chan =
+  Printf.fprintf chan "%s"
+
 let main () =
   let json = Yojson.SafePos.from_channel stdin in
-  List.iter (fun (pos, s) ->
-    let open Yojson.SafePos in
-    let fnamestr =
-      match pos.file_name with
-      | None -> ""
-      | Some(x) -> " in '" ^ x ^ "'"
-    in
-    let lnum1 = pos.start_line in
-    let lnum2 = pos.end_line in
-    if lnum1 = lnum2 then
-      Printf.printf "%s (line %d, column %d-%d%s)\n"
-        s lnum1 pos.start_column pos.end_column fnamestr
-    else
-      Printf.printf "%s (line %d column %d to line %d, column %d)\n"
-        s lnum1 pos.start_column lnum2 pos.end_column
-  ) (extract_titles json)
+  List.iter (show ~print:print_string) (extract_titles json)
 
 let () = main ()
