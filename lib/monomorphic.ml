91e45e984c6e14b42ef9e2239126924252ed444e
Marek Kubica <marek@xivilization.net>
2019-01-24 23:03:32 +0100

    Reformat the code to the point where it is halfway readable

--- lib/monomorphic.ml
+++ lib/monomorphic.ml
@@ -2,106 +2,98 @@ let rec pp fmt =
   function
   | `Null -> Format.pp_print_string fmt "`Null"
   | `Bool x ->
-      (Format.fprintf fmt "`Bool (@[<hov>";
-       (Format.fprintf fmt "%B") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Bool (@[<hov>";
+    Format.fprintf fmt "%B" x;
+    Format.fprintf fmt "@])"
 #ifdef INT
   | `Int x ->
-      (Format.fprintf fmt "`Int (@[<hov>";
-       (Format.fprintf fmt "%d") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Int (@[<hov>";
+    Format.fprintf fmt "%d" x;
+    Format.fprintf fmt "@])"
 #endif
 #ifdef INTLIT
   | `Intlit x ->
-      (Format.fprintf fmt "`Intlit (@[<hov>";
-       (Format.fprintf fmt "%S") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Intlit (@[<hov>";
+    Format.fprintf fmt "%S" x;
+    Format.fprintf fmt "@])"
 #endif
 #ifdef FLOAT
   | `Float x ->
-      (Format.fprintf fmt "`Float (@[<hov>";
-       (Format.fprintf fmt "%F") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Float (@[<hov>";
+    Format.fprintf fmt "%F" x;
+    Format.fprintf fmt "@])"
 #endif
 #ifdef FLOATLIT
   | `Floatlit x ->
-      (Format.fprintf fmt "`Floatlit (@[<hov>";
-       (Format.fprintf fmt "%S") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Floatlit (@[<hov>";
+    Format.fprintf fmt "%S" x;
+    Format.fprintf fmt "@])"
 #endif
 #ifdef STRING
   | `String x ->
-      (Format.fprintf fmt "`String (@[<hov>";
-       (Format.fprintf fmt "%S") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`String (@[<hov>";
+    Format.fprintf fmt "%S" x;
+    Format.fprintf fmt "@])"
 #endif
 #ifdef STRINGLIT
   | `Stringlit x ->
-      (Format.fprintf fmt "`Stringlit (@[<hov>";
-       (Format.fprintf fmt "%S") x;
-       Format.fprintf fmt "@])")
+    Format.fprintf fmt "`Stringlit (@[<hov>";
+    Format.fprintf fmt "%S" x;
+    Format.fprintf fmt "@])"
 #endif
-  | `Assoc x ->
-      (Format.fprintf fmt "`Assoc (@[<hov>";
-       ((fun x ->
-           Format.fprintf fmt "@[<2>[";
-           ignore
-             (List.fold_left
-                (fun sep ->
-                   fun x ->
-                     if sep then Format.fprintf fmt ";@ ";
-                     ((fun (a0, a1) ->
-                         Format.fprintf fmt "(@[";
-                         ((Format.fprintf fmt "%S") a0;
-                          Format.fprintf fmt ",@ ";
-                          (pp fmt) a1);
-                         Format.fprintf fmt "@])")) x;
-                     true) false x);
-           Format.fprintf fmt "@,]@]")) x;
-       Format.fprintf fmt "@])")
-  | `List x ->
-      (Format.fprintf fmt "`List (@[<hov>";
-       ((fun x ->
-           Format.fprintf fmt "@[<2>[";
-           ignore
-             (List.fold_left
-                (fun sep ->
-                   fun x ->
-                     if sep then Format.fprintf fmt ";@ ";
-                     (pp fmt) x;
-                     true) false x);
-           Format.fprintf fmt "@,]@]")) x;
-       Format.fprintf fmt "@])")
+  | `Assoc xs ->
+    Format.fprintf fmt "`Assoc (@[<hov>";
+    Format.fprintf fmt "@[<2>[";
+    ignore (List.fold_left
+      (fun sep (key, value) ->
+        if sep then
+          Format.fprintf fmt ";@ ";
+          Format.fprintf fmt "(@[";
+          Format.fprintf fmt "%S" key;
+          Format.fprintf fmt ",@ ";
+          pp fmt value;
+          Format.fprintf fmt "@])";
+          true) false xs);
+    Format.fprintf fmt "@,]@]";
+    Format.fprintf fmt "@])"
+  | `List xs ->
+    Format.fprintf fmt "`List (@[<hov>";
+    Format.fprintf fmt "@[<2>[";
+    ignore (List.fold_left
+      (fun sep x ->
+        if sep then
+          Format.fprintf fmt ";@ ";
+          pp fmt x;
+          true) false xs);
+    Format.fprintf fmt "@,]@]";
+    Format.fprintf fmt "@])"
 #ifdef TUPLE
-  | `Tuple x ->
-      (Format.fprintf fmt "`Tuple (@[<hov>";
-       ((fun x ->
-           Format.fprintf fmt "@[<2>[";
-           ignore
-             (List.fold_left
-                (fun sep ->
-                   fun x ->
-                     if sep then Format.fprintf fmt ";@ ";
-                     (pp fmt) x;
-                     true) false x);
-           Format.fprintf fmt "@,]@]")) x;
-       Format.fprintf fmt "@])")
+  | `Tuple tup ->
+    Format.fprintf fmt "`Tuple (@[<hov>";
+    Format.fprintf fmt "@[<2>[";
+    ignore (List.fold_left
+      (fun sep e ->
+         if sep then
+           Format.fprintf fmt ";@ ";
+           pp fmt e;
+           true) false tup);
+    Format.fprintf fmt "@,]@]";
+    Format.fprintf fmt "@])"
 #endif
 #ifdef VARIANT
-  | `Variant x ->
-      (Format.fprintf fmt "`Variant (@[<hov>";
-       ((fun (a0, a1) ->
-           Format.fprintf fmt "(@[";
-           ((Format.fprintf fmt "%S") a0;
-            Format.fprintf fmt ",@ ";
-            ((function
-              | None -> Format.pp_print_string fmt "None"
-              | Some x ->
-                  (Format.pp_print_string fmt "(Some ";
-                   (pp fmt) x;
-                   Format.pp_print_string fmt ")"))) a1);
-           Format.fprintf fmt "@])")) x;
-       Format.fprintf fmt "@])")
+  | `Variant (name, value) ->
+    Format.fprintf fmt "`Variant (@[<hov>";
+    Format.fprintf fmt "(@[";
+    Format.fprintf fmt "%S" name;
+    Format.fprintf fmt ",@ ";
+    (match value with
+      | None -> Format.pp_print_string fmt "None"
+      | Some x ->
+        Format.pp_print_string fmt "(Some ";
+        pp fmt x;
+        Format.pp_print_string fmt ")");
+    Format.fprintf fmt "@])";
+    Format.fprintf fmt "@])"
 #endif
 
 let show x =
