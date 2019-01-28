1a2c620918371f5cb27bb89a6f0c33ed01eb4084
Marek Kubica <marek@xivilization.net>
2019-01-28 14:15:41 +0100

    Add clearer error message in deprecation warning

--- lib/type.ml
+++ lib/type.ml
@@ -53,7 +53,7 @@ All possible cases defined in Yojson:
 	    Syntax: [<"Foo">] or [<"Bar":123>].
 *)
 
-type json = t [@@deprecated "renamed to t"]
+type json = t [@@deprecated "json types are being renamed and will be removed in the next Yojson major version. Use type t instead"]
 (**
  * Compatibility type alias for type `t`
  *)
