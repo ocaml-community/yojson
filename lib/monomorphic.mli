171dadeb81bf0d82a4832c02bee36b753ca8130f
Marek Kubica <marek@xivilization.net>
2019-01-28 14:08:53 +0100

    Clear up documentation on duplicate keys

--- lib/monomorphic.mli
+++ lib/monomorphic.mli
@@ -7,6 +7,7 @@ val show : t -> string
 val equal : t -> t -> bool
   (** [equal a b] is the monomorphic equality.
       Determines whether two JSON values are considered equal. In the case of
-      JSON objects, the order of the keys does not matter. In case of duplicate
-      keys, these have to exist in both objects to be considered equal.
+      JSON objects, the order of the keys does not matter, except for
+      duplicate keys which will be considered equal as long as they are in the
+      same input order.
     *)
