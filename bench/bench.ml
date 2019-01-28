0dc72f4a65dc48bab957002a982f428f1ff320af
Marek Kubica <marek@xivilization.net>
2019-01-28 19:01:50 +0100

    Remove json_io from benchmark

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,5 +1,3 @@
-open Printf
-
 let data =
   let l = ref [] in
   try
@@ -10,7 +8,6 @@ let data =
   with End_of_file -> String.concat "\n" (List.rev !l)
 
 let yojson_data = Yojson.Safe.from_string data
-let jsonwheel_data = Json_io.json_of_string data
 
 let n = 10_000
 
@@ -24,30 +21,13 @@ let yojson_wr_loop () =
     ignore (Yojson.Safe.to_string yojson_data)
   done
 
-let jsonwheel_rd_loop () =
-  for i = 1 to n do
-    ignore (Json_io.json_of_string data)
-  done
-
-let jsonwheel_wr_loop () =
-  for i = 1 to n do
-    ignore (Json_io.string_of_json ~compact:true jsonwheel_data)
-  done
-
 let time msg f =
   let t1 = Unix.gettimeofday () in
   f ();
   let t2 = Unix.gettimeofday () in
-  printf "%s: %.3f\n%!" msg (t2 -. t1)
+  Printf.printf "%s: %.3f\n%!" msg (t2 -. t1)
 
 let () =
   time "rd yojson" yojson_rd_loop;
-  time "rd json-wheel" jsonwheel_rd_loop;
-  time "rd yojson" yojson_rd_loop;
-  time "rd json-wheel" jsonwheel_rd_loop;
 
   time "wr yojson" yojson_wr_loop;
-  time "wr json-wheel" jsonwheel_wr_loop;
-  time "wr yojson" yojson_wr_loop;
-  time "wr json-wheel" jsonwheel_wr_loop
-  
