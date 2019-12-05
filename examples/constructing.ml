2411aa8f749c38fd8fd69b2387c1a236e692ad8f
Gargi Sharma <gs051095@gmail.com>
2019-12-05 18:16:18 +0100

    Add an example for constructing json

---
+++ examples/constructing.ml
@@ -0,0 +1,34 @@
+(*
+  dune exec examples/constructing.exe <<EOF
+{
+  "id": "398eb027",
+  "name": "John Doe",
+  "pages": [
+    {
+      "id": 1,
+      "title": "The Art of Flipping Coins",
+    }
+  ]
+}
+EOF
+*)
+
+open Yojson.Basic.Util
+
+let json_output =
+  `Assoc
+    [
+      ("id", `String "398eb027");
+      ("name", `String "John Doe");
+      ( "pages",
+        `Assoc
+          [ ("id", `Int 1); ("title", `String "The Art of Flipping Coins") ] );
+    ]
+
+let main () =
+  let oc = stdout in
+  Yojson.Basic.pretty_to_channel oc json_output;
+  output_string oc "\n";
+  close_out oc
+
+let () = main ()
