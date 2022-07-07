40573ee2c0f49491e837cd04d851ced04404415c
Ambre Austen Suhamy <ambre@tarides.com>
2022-07-07 12:09:15 +0200

    Cleaning code for readability, no logic change

--- bench/conversions.ml
+++ bench/conversions.ml
@@ -1,9 +1,9 @@
-open Core_bench_internals.Simplified_benchmark
+module SB = Core_bench_internals.Simplified_benchmark
 
-let input_results in_chan = Sexplib.Sexp.input_sexp in_chan |> Results.t_of_sexp
+let input_results in_chan =
+  Sexplib.Sexp.input_sexp in_chan |> SB.Results.t_of_sexp
 
 let metrics name value unit_name =
-  let open Yojson.Safe in
   `Assoc
     [
       ("name", `String name);
@@ -12,41 +12,34 @@ let metrics name value unit_name =
     ]
 
 let extract full_name results =
-  let open Result in
-  let open Yojson.Safe in
-  List.iter
-    (fun res ->
-      let json =
-        `Assoc
-          [
-            ( "results",
-              `List
-                [
-                  `Assoc
-                    [
-                      ("name", `String full_name);
-                      ( "metrics",
-                        `List
-                          [
-                            metrics
-                              (res.full_benchmark_name ^ " (time)")
-                              res.time_per_run_nanos "ns";
-                            metrics
-                              (res.full_benchmark_name ^ " (memory)/minor")
-                              res.minor_words_per_run
-                              "words";
-                            metrics
-                              (res.full_benchmark_name ^ " (memory)/major")
-                              res.major_words_per_run
-                              "words";
-                          ] );
-                    ];
-                ] );
-          ]
-      in
-      to_channel stdout json;
-      print_newline ())
-    results
+  results
+  |> List.map (fun (res : SB.Result.t) ->
+         `Assoc
+           [
+             ( "results",
+               `List
+                 [
+                   `Assoc
+                     [
+                       ("name", `String full_name);
+                       ( "metrics",
+                         `List
+                           [
+                             metrics
+                               (res.full_benchmark_name ^ " (time)")
+                               res.time_per_run_nanos "ns";
+                             metrics
+                               (res.full_benchmark_name ^ " (memory)/minor")
+                               res.minor_words_per_run "words";
+                             metrics
+                               (res.full_benchmark_name ^ " (memory)/major")
+                               res.major_words_per_run "words";
+                           ] );
+                     ];
+                 ] );
+           ])
+  |> List.to_seq
+  |> Yojson.Safe.seq_to_channel stdout
 
 let () =
   let full_name = Sys.argv.(1) in
