68dbc4a8b1a75634dd392d2cf13502c390942138
Marek Kubica <marek@xivilization.net>
2022-07-22 17:11:03 +0200

    Also test `map_string`

--- test/test_read.ml
+++ test/test_read.ml
@@ -19,13 +19,13 @@ let unquoted_from_string () =
     (Yojson.Safe.from_string Fixtures.unquoted_json)
 
 let unquoted_from_lexbuf () =
-  let lexbuf = Lexing.from_string "{foo: null, bar: null}" in
+  let lexbuf = Lexing.from_string {|{foo: null, bar: "hello"}|} in
   let lexer_state = Yojson.init_lexer () in
 
   let ident_expected expectation reference start len =
     let identifier = String.sub reference start len in
     Alcotest.(check string)
-      (Format.asprintf "Reference %s start %d len %d matches %s" reference start len expectation)
+      (Format.asprintf "Reference '%s' start %d len %d matches '%s'" reference start len expectation)
       expectation
       identifier;
     ()
@@ -33,9 +33,11 @@ let unquoted_from_lexbuf () =
   let skip_over f =
     f lexer_state lexbuf
   in
-  let map_ident f =
-    Yojson.Safe.map_ident lexer_state f lexbuf
+  let map_f mapper f =
+    mapper lexer_state f lexbuf
   in
+  let map_ident = map_f Yojson.Safe.map_ident in
+  let map_string = map_f Yojson.Safe.map_string in
 
   skip_over Yojson.Safe.read_lcurl;
   map_ident (ident_expected "foo");
@@ -47,7 +49,11 @@ let unquoted_from_lexbuf () =
   map_ident (ident_expected "bar");
   skip_over Yojson.Safe.read_colon;
   skip_over Yojson.Safe.read_space;
-  skip_over Yojson.Safe.read_null;
+
+  let variant = skip_over Yojson.Safe.start_any_variant in
+  Alcotest.(check Testable.variant_kind) "String starts with double quote" `Double_quote variant;
+
+  map_string (ident_expected "hello");
 
   (match Yojson.Safe.read_object_end lexbuf with
   | _ -> Alcotest.fail "Object end expected but did not happen"
