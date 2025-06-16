4165acabae9f144682deeb451a0114b5650a04a1
John Whitington <john@coherentgraphics.co.uk>
2025-06-16 13:26:28 +0100

    Process with ocamlformat

--- examples/introduction.ml
+++ examples/introduction.ml
@@ -7,10 +7,11 @@ let json_string =
 (* Print as parse tree *)
 let print_parse_tree () =
   Format.printf "%a\n" Yojson.Safe.pp (Yojson.Safe.from_string json_string)
-  
+
 (* Print as JSON *)
 let prettyprint () =
-  Printf.printf "%s\n" (Yojson.Safe.pretty_to_string (Yojson.Safe.from_string json_string))
+  Printf.printf "%s\n"
+    (Yojson.Safe.pretty_to_string (Yojson.Safe.from_string json_string))
 
 (* Read some JSON from a file, add up all numbers, and print. *)
 let sum_floats = List.fold_left ( +. ) 0.
@@ -22,7 +23,7 @@ let rec sum_json : Yojson.Safe.t -> float = function
   | `List l -> sum_floats (List.map sum_json l)
   | `Assoc l -> sum_floats (List.map sum_json (List.map snd l))
   | _ -> failwith "unexpected construct in sum_json"
-  
+
 let sum filename =
   Printf.printf "%f\n" (sum_json (Yojson.Safe.from_file filename))
 
@@ -35,18 +36,18 @@ let rec reverse_json = function
 let reverse in_filename out_filename =
   let json = reverse_json (Yojson.Safe.from_file in_filename) in
   let fh = open_out_bin out_filename in
-    Yojson.Safe.pretty_to_channel fh json;
-    close_out fh
+  Yojson.Safe.pretty_to_channel fh json;
+  close_out fh
 
 (* You can use the file introduction.json as an example input for the "sum" and
    "reverse" examples. *)
 let () =
   match Sys.argv with
-  | [|_; "print_parse_tree"|] -> print_parse_tree ()
-  | [|_; "prettyprint"|] -> prettyprint ()
-  | [|_; "sum"; filename|] -> sum filename
-  | [|_; "reverse"; in_filename; out_filename|] -> reverse in_filename out_filename
+  | [| _; "print_parse_tree" |] -> print_parse_tree ()
+  | [| _; "prettyprint" |] -> prettyprint ()
+  | [| _; "sum"; filename |] -> sum filename
+  | [| _; "reverse"; in_filename; out_filename |] ->
+      reverse in_filename out_filename
   | _ ->
-      Printf.eprintf
-        "%s: unknown command line\n"
+      Printf.eprintf "%s: unknown command line\n"
         (if Array.length Sys.argv > 0 then Sys.argv.(0) else "")
